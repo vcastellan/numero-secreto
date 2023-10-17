38be694cbb220fbb4eb311db41c17d83808db055
vcastellan <victor_castellan@hotmail.com>
2023-10-17 12:17:44 -0300

    criar variaveis e adicionar mensagem de acerto

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo dos Números Secretos!');
-let numeroSecreto = 29;
-prompt ('Escolha um número entre 1 e 30')
+let numeroSecreto = 5;
+let chute = prompt ('Escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+}
